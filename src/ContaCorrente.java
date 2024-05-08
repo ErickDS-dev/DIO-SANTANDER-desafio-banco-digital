11b7b0b546c2713c620a706e0bf9076103076fa0
ErickDs-dev <erycdiogenes@gmail.com>
2024-05-08 12:37:15 -0300

    encapsulation

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,7 +1,20 @@
 public class ContaCorrente {
-    public int agencia;
-    public int numero;
-    public double saldo;
+    private int agencia;
+    private int numero;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    private double saldo;
     public void sacar(){
 
     }
